e1397d43aa6cc3287811b5e7ea3bea1bcc5923e4
Francis <brakay1136@gmail.com>
2024-12-01 12:38:27 -0800

    improved layout for mobile view

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const hamburgerManu = document.querySelector(".icon-manu");
 const closeManu = document.querySelector(".close-icon");
 const mobileNavLinks = document.querySelector('.mobileNavLinks')
+const overlay = document.querySelector('.overlay')
 
 hamburgerManu.addEventListener('click', () => {
     mobileNavLinks.classList.toggle('openDrawer');
+    overlay.style.display = "block";
 
 })
 closeManu.addEventListener('click', () => {
     mobileNavLinks.classList.toggle('openDrawer');
+    overlay.style.display = "none";
+
 
 })
